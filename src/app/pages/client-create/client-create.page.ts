81e4e7e839727156b6ccbf0fb00d4bb88abf3958
Miten Chauhan <miten.chauhan@safer.me>
2019-06-09 10:01:25 +1200

    Client create page

--- src/app/pages/client-create/client-create.page.ts
+++ src/app/pages/client-create/client-create.page.ts
@@ -1,4 +1,8 @@
+import { LoadingController } from '@ionic/angular';
+import { ClientService } from './../../services/client.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-client-create',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientCreatePage implements OnInit {
 
-  constructor() { }
+  public clientCreateForm: FormGroup;
+
+  constructor(
+    private clientService: ClientService,
+    private formbuilder: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.clientCreateForm = this.formbuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      startingWeight: ['', Validators.required]
+    });
+  }
+
+  public async clientCreate(): Promise<void> {
+    if (this.clientCreateForm.valid) {
+      const loading = await this.loadingCtrl.create();
+      loading.present();
+      const { name, email, startingWeight } = this.clientCreateForm.value;
+      try {
+        await this.clientService.createClient(name, email, startingWeight);
+        this.router.navigateByUrl('/home');
+      } catch (error) {
+        console.error(error);
+      } finally {
+        loading.dismiss();
+      }
+    } else {
+      console.error(this.clientCreateForm.value);
+    }
   }
 
 }
