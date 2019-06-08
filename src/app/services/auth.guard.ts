060d14d5f9cf6f911bbf6db9505b278da2ccc322
Miten Chauhan <miten.chauhan@safer.me>
2019-06-08 17:13:50 +1200

    Basic auth guard

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
+export class AuthGuard implements CanActivate {
+
+  constructor() {}
+
+  canActivate() {
+    return true;
+  }
 }
