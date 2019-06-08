ab2744a687777478e7c1a71a78cf7ac12aa32bb4
Miten Chauhan <miten.chauhan@safer.me>
2019-06-08 15:58:16 +1200

    Create auth service functions

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,54 @@
+import { User } from './../interfaces/user';
+import { UserService } from './user.service';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  public currentUser: firebase.User;
+
+  constructor(private afAuth: AngularFireAuth, private userService: UserService) {
+    this.afAuth.authState.subscribe((user: firebase.User) => {
+      if (user) {
+        this.currentUser = user;
+      }
+    });
+  }
+
+  public get currentUserId(): string {
+    return this.currentUser.uid;
+  }
+
+  public login(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  public logout(): Promise<void> {
+    return this.afAuth.auth.signOut();
+  }
+
+  public passwordReset(email: string): Promise<void> {
+    return this.afAuth.auth.sendPasswordResetEmail(email);
+  }
+
+  public signup(email: string, password: string, fullname: string): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        const userCredential: firebase.auth.UserCredential = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+        const user: User = {
+          id: userCredential.user.uid,
+          fullname,
+          email,
+          admin: true
+        };
+        this.userService.createUser(user);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { User } from './../interfaces/user';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly rootPath: string = 'userProfile';
+
+  constructor(private firestore: AngularFirestore) { }
+
+  public createUser(user: User): Promise<void> {
+    return this.firestore.doc<User>(`${this.rootPath}/${user.id}`).set(user);
+  }
+
+  public isAdmin(userId: string): Promise<boolean> {
+    return new Promise<boolean>(async (resolve, reject) => {
+      try {
+        const user: User = await this.firestore
+          .doc<User>(`${this.rootPath}/${userId}`)
+          .valueChanges()
+          .toPromise();
+        resolve(user.admin);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+}
