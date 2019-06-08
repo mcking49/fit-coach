6c7d9205aa5d0d09918d905a8a57d66ec56ab437
Miten Chauhan <miten.chauhan@safer.me>
2019-06-08 17:46:33 +1200

    Create password reset page

--- src/app/pages/password-reset/password-reset.page.ts
+++ src/app/pages/password-reset/password-reset.page.ts
@@ -1,4 +1,8 @@
+import { AuthService } from './../../services/auth.service';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-password-reset',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordResetPage implements OnInit {
 
-  constructor() { }
+  public passwordResetForm: FormGroup;
+
+  constructor(
+    private alertCtrl: AlertController,
+    private authService: AuthService,
+    private formbuilder: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private router: Router,
+    private toastCtrl: ToastController
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  public async resetPassword(): Promise<void> {
+    if (this.passwordResetForm.valid) {
+      const loading = await this.loadingCtrl.create();
+      loading.present();
+
+      const { email } = this.passwordResetForm.value;
+
+      try {
+        await this.authService.passwordReset(email);
+        const toast = await this.toastCtrl.create({
+          message: 'A reset link has been sent to your email',
+          duration: 2000
+        });
+        toast.present();
+        this.router.navigateByUrl('/login');
+      } catch (error) {
+        const alert = await this.alertCtrl.create({
+          message: error.message,
+          buttons: [{text: 'Ok', role: 'cancel'}]
+        });
+        alert.present();
+      } finally {
+        loading.dismiss();
+      }
+    } else {
+      const errorToast = await this.toastCtrl.create({
+        message: 'ERROR: Please enter a valid email',
+        duration: 3000
+      });
+      errorToast.present();
+    }
+  }
+
+  private initForm() {
+    this.passwordResetForm = this.formbuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.email, Validators.required])
+      ]
+    });
   }
 
 }
