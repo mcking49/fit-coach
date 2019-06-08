1fbad3b5c69e7ac5a7d0361a24eae6545603c0b0
Miten Chauhan <miten.chauhan@safer.me>
2019-06-08 18:16:10 +1200

    Create the signup page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,8 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './../../services/auth.service';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  public signupForm: FormGroup;
+
+  constructor(
+    private alertCtrl: AlertController,
+    private authService: AuthService,
+    private formbuilder: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private router: Router,
+    private toastCtrl: ToastController
+  ) { }
 
   ngOnInit() {
+    this.initSignupForm();
+  }
+
+  public async signup(): Promise<void> {
+    if (this.signupForm.valid) {
+      const loading = await this.loadingCtrl.create();
+      loading.present();
+
+      const { name, email, password } = this.signupForm.value;
+
+      try {
+        await this.authService.signup(email, password, name);
+        this.showToast('Signup successful!');
+        this.router.navigateByUrl('/home');
+      } catch (error) {
+        const alert = await this.alertCtrl.create({
+          message: error.message,
+          buttons: [{text: 'Ok', role: 'cancel'}]
+        });
+        alert.present();
+      } finally {
+        loading.dismiss();
+      }
+    } else {
+      this.showToast('ERROR: please fill out all the fields', 3000);
+    }
+  }
+
+  private async showToast(message: string, duration: number = 2000): Promise<void> {
+    const toast = await this.toastCtrl.create({message, duration});
+    toast.present();
+  }
+
+  private initSignupForm(): void {
+    this.signupForm = this.formbuilder.group({
+      name: [
+        '',
+        Validators.required
+      ],
+      email: [
+        '',
+        Validators.compose([Validators.email, Validators.required])
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.minLength(6), Validators.required])
+      ]
+    });
   }
 
 }
