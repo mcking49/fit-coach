fa85b56c86c3cb12129043443a263efbb79ed7ee
Miten Chauhan <miten.chauhan@safer.me>
2019-06-09 11:23:05 +1200

    Create client cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createClientAccount = functions.firestore
+  .document('userProfile/{userId}/clientList/{clientId}')
+  .onCreate(async (snapshot, context) => {
+    const userCredential = {
+      email: snapshot.data()!.email,
+      displayName: snapshot.data()!.fullname,
+      uid: context.params.clientId,
+      password: '121212'
+    };
+
+    try {
+      const userRecord: admin.auth.UserRecord = await admin.auth().createUser(userCredential);
+      console.log('... userRecord created');
+      const user = {
+        fullname: userRecord.displayName,
+        id: userRecord.uid,
+        email: userRecord.email,
+        coachId: context.params.userId,
+        admin: false,
+        startingWeight: snapshot.data()!.startingWeight
+      }
+      console.log('... creating user profile');
+      return admin.firestore().doc(`userProfile/${user.id}`).set(user);
+    } catch (error) {
+      console.error('--- ERROR: Failed to create a user: ', error);
+      return error;
+    }
+  });
--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -4,4 +4,5 @@ export interface User {
   email: string;
   admin: boolean;
   startingWeight?: number;
+  coachId?: string;
 }
