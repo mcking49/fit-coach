772d7a2a6478258c32fcd40cc1cdbd7432111b1b
Miten Chauhan <miten.chauhan@safer.me>
2019-06-08 14:22:02 +1200

    Import modules

--- src/app/pages/client-detail/client-detail.module.ts
+++ src/app/pages/client-detail/client-detail.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 
 import { ClientDetailPage } from './client-detail.page';
 
+import { MomentModule } from 'ngx-moment';
+
 const routes: Routes = [
   {
     path: '',
@@ -19,7 +21,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MomentModule
   ],
   declarations: [ClientDetailPage]
 })
